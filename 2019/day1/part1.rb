953117538982dc74c64c360257f575e1cce5b74d
dadams <adams.t.david@gmail.com>
2019-12-05 22:02:03 -0700

    add answer for 2019 day 1

---
+++ 2019/day1/part1.rb
@@ -0,0 +1,7 @@
+input_data = File.read("input.txt").split
+
+def find_fuel(modules)
+  modules.reduce(0) { |sum, mass| sum + mass.to_i / 3 - 2 }
+end
+
+puts(find_fuel(input_data))
---
+++ 2019/day1/part2.rb
@@ -0,0 +1,17 @@
+input_data = File.read("input.txt").split
+
+def get_total_fuel(mass)
+  fuel = mass / 3 - 2
+
+  if fuel < 0
+    return 0
+  end
+
+  return fuel + get_total_fuel(fuel)
+end
+
+def find_fuel(modules)
+  modules.reduce(0) { |sum, mass| sum + get_total_fuel(mass.to_i) }
+end
+
+puts(find_fuel(input_data))
