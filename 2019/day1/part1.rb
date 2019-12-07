90863cde68fcc8dbd410791db62f63cee5f65873
dadams <adams.t.david@gmail.com>
2019-12-07 13:03:06 -0700

    use better ruby syntax

--- 2019/day1/part1.rb
+++ 2019/day1/part1.rb
@@ -4,4 +4,4 @@ def find_fuel(modules)
   modules.reduce(0) { |sum, mass| sum + mass.to_i / 3 - 2 }
 end
 
-puts(find_fuel(input_data))
+puts find_fuel(input_data)
--- 2019/day1/part2.rb
+++ 2019/day1/part2.rb
@@ -14,4 +14,4 @@ def find_fuel(modules)
   modules.reduce(0) { |sum, mass| sum + get_total_fuel(mass.to_i) }
 end
 
-puts(find_fuel(input_data))
+puts find_fuel(input_data)
