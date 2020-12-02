4e4af8ba58a8cd15a4e9aaf2cd54870639826087
dadams <adams.t.david@gmail.com>
2020-12-02 16:03:54 -0700

    2020 day 2 answer

---
+++ 2020/day2/part1.rb
@@ -0,0 +1,22 @@
+passwords = File.read('input.txt').split("\n").map do |password_line|
+  minmax, policy_letter, password = password_line
+    .tr(':', '')
+    .split(' ')
+  letter_min, letter_max = minmax.split('-')
+
+  {
+    letter_min: letter_min.to_i,
+    letter_max: letter_max.to_i,
+    policy_letter: policy_letter,
+    password: password
+  }
+end
+
+def validate_password(letter_min:, letter_max:, policy_letter:, password:)
+  occurences = password.count(policy_letter)
+  occurences >= letter_min && occurences <= letter_max
+end
+
+valid_passwords = passwords.select { |password| validate_password(**password)}
+
+puts valid_passwords.size
---
+++ 2020/day2/part2.rb
@@ -0,0 +1,22 @@
+passwords = File.read('input.txt').split("\n").map do |password_line|
+  minmax, policy_letter, password = password_line
+    .tr(':', '')
+    .split(' ')
+  position_one, position_two = minmax.split('-')
+
+  {
+    position_one: position_one.to_i,
+    position_two: position_two.to_i,
+    policy_letter: policy_letter,
+    password: password
+  }
+end
+
+def validate_password(position_one:, position_two:, policy_letter:, password:)
+  positions = password[position_one - 1] + password[position_two - 1]
+  positions.count(policy_letter) == 1
+end
+
+valid_passwords = passwords.select { |password| validate_password(**password)}
+
+puts valid_passwords.size
