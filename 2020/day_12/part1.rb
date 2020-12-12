187418055b580be5b3b60a2b9ff3af6b60076cf5
dadams <adams.t.david@gmail.com>
2020-12-12 15:38:50 -0700

    add 2020 day 12 solution

---
+++ 2020/day_12/part1.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+instructions = File
+  .readlines('./input.txt', chomp: true)
+  .map { |line| [line[0], line[1..].to_i] }
+
+def move_forward(x_pos, y_pos, direction, distance)
+  case direction
+  when 0
+    [x_pos, y_pos + distance]
+  when 90
+    [x_pos + distance, y_pos]
+  when 180
+    [x_pos, y_pos - distance]
+  when 270
+    [x_pos - distance, y_pos]
+  else
+    raise 'ERROR'
+  end
+end
+
+def change_direction(new_direction)
+  return 0 if new_direction == 360
+  return new_direction - 360 if new_direction > 360
+  return new_direction + 360 if new_direction.negative?
+
+  new_direction
+end
+
+current_position = [0, 0]
+current_direction = 90
+
+instructions.each do |direction, distance|
+  current_x = current_position[0]
+  current_y = current_position[1]
+
+  case direction
+  when 'N'
+    current_position = [current_x, current_y + distance]
+  when 'S'
+    current_position = [current_x, current_y - distance]
+  when 'E'
+    current_position = [current_x + distance, current_y]
+  when 'W'
+    current_position = [current_x - distance, current_y]
+  when 'L'
+    current_direction = change_direction(current_direction - distance)
+  when 'R'
+    current_direction = change_direction(current_direction + distance)
+  when 'F'
+    current_position = move_forward(current_x, current_y, current_direction, distance)
+  end
+end
+
+answer = current_position[0].abs + current_position[1].abs
+puts answer
---
+++ 2020/day_12/part2.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+instructions = File
+  .readlines('./input.txt', chomp: true)
+  .map { |line| [line[0], line[1..].to_i] }
+
+def rotate_waypoint(x_pos, y_pos, direction)
+  radians = direction * (Math::PI / 180)
+  rotated_x = (x_pos * Math.cos(radians)) - (y_pos * Math.sin(radians)).round
+  rotated_y = (x_pos * Math.sin(radians)) + (y_pos * Math.cos(radians)).round
+  [rotated_x, rotated_y]
+end
+
+waypoint_position = [10, 1]
+ship_position = [0, 0]
+
+instructions.each do |direction, distance|
+  waypoint_x = waypoint_position[0]
+  waypoint_y = waypoint_position[1]
+  ship_x = ship_position[0]
+  ship_y = ship_position[1]
+
+  case direction
+  when 'N'
+    waypoint_position = [waypoint_x, waypoint_y + distance]
+  when 'S'
+    waypoint_position = [waypoint_x, waypoint_y - distance]
+  when 'E'
+    waypoint_position = [waypoint_x + distance, waypoint_y]
+  when 'W'
+    waypoint_position = [waypoint_x - distance, waypoint_y]
+  when 'L'
+    waypoint_position = rotate_waypoint(waypoint_x, waypoint_y, distance)
+  when 'R'
+    waypoint_position = rotate_waypoint(waypoint_x, waypoint_y, -distance)
+  when 'F'
+    ship_position = [ship_x + (waypoint_x * distance), ship_y + (waypoint_y * distance)]
+  end
+end
+
+answer = (ship_position[0].abs + ship_position[1].abs).to_i
+p ship_position
+puts answer
