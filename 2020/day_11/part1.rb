76f7bdc7397153b9c67355f9afe6384443594c91
dadams <adams.t.david@gmail.com>
2020-12-12 11:14:24 -0700

    add 2020 day 11 solution

---
+++ 2020/day_11/part1.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+GRID = File.read('./input.txt').split("\n").map {|line| line.split('') }
+
+def print_grid(grid)
+  grid.each do |row|
+    p row.join
+  end
+  nil
+end
+
+def calculate_seat(row, column, grid)
+  seat = grid[row][column]
+
+  return seat if seat == '.'
+
+  top_row = row - 1 >= 0 ? row - 1 : 1000000
+  left_column = column - 1 >= 0 ? column - 1 : 1000000
+  top_left = grid.dig(top_row, left_column)
+  top = grid.dig(top_row, column)
+  top_right = grid.dig(top_row, column + 1)
+  middle_left = grid.dig(row, left_column)
+  middle_right = grid.dig(row, column + 1)
+  bottom_left = grid.dig(row + 1, left_column)
+  bottom = grid.dig(row + 1, column)
+  bottom_right = grid.dig(row + 1, column + 1)
+
+  occupied_adjacent_seats = [top_left, top, top_right, middle_left, middle_right, bottom_left, bottom, bottom_right].count('#')
+
+  if seat == 'L' && occupied_adjacent_seats == 0
+    return '#'
+  end
+
+  if seat == '#' && occupied_adjacent_seats >= 4
+    return 'L'
+  end
+
+  seat
+end
+
+def calculate_seats(grid)
+  occupied_seats = 0
+  new_grid = Marshal.load(Marshal.dump(grid))
+
+  grid.each_with_index do |row, row_index|
+    row.each_with_index do |column, column_index|
+      new_seat = calculate_seat(row_index, column_index, grid)
+      new_grid[row_index][column_index] = new_seat
+      occupied_seats += 1 if new_seat == '#'
+    end
+  end
+
+  [occupied_seats, new_grid]
+end
+
+prev_occupied_seats = -1
+occupied_seats = 0
+grid = GRID
+
+while prev_occupied_seats != occupied_seats
+  prev_occupied_seats = occupied_seats
+  occupied_seats, grid = calculate_seats(grid)
+end
+
+p occupied_seats
---
+++ 2020/day_11/part2.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+GRID = File.read('./input.txt').split("\n").map {|line| line.split('') }
+GRID_ROW_MAX = GRID.size - 1
+GRID_COLUMN_MAX = GRID[0].size - 1
+
+# top left = [-1, -1] top = [-1, 0] top right = [-1, +1]
+# middle left = [0, -1] middle right = [0, +1]
+# bottom left = [+1, -1] bottom = [+1, 0] bottom right = [+1, +1]
+DELTAS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+def in_grid(row, column)
+  row >= 0 && row <= GRID_ROW_MAX && column >= 0 && column <= GRID_COLUMN_MAX
+end
+
+def scan_diagonal(row, column, grid, delta)
+  has_occupied_seat = false
+  row_change, column_change = delta
+  new_row = row + row_change
+  new_column = column + column_change
+
+  while in_grid(new_row, new_column)
+    seat = grid.dig(new_row, new_column)
+
+    if seat == '#'
+      has_occupied_seat = true
+      break
+    elsif seat == 'L'
+      break
+    end
+
+    new_row += row_change
+    new_column += column_change
+  end
+
+  has_occupied_seat
+end
+
+def calculate_seat(row, column, grid)
+  seat = grid[row][column]
+
+  return seat if seat == '.'
+
+  occupied_adjacent_seats = DELTAS.map { |delta| scan_diagonal(row, column, grid, delta) }.count(true)
+
+  if seat == 'L' && occupied_adjacent_seats == 0
+    return '#'
+  end
+
+  if seat == '#' && occupied_adjacent_seats >= 5
+    return 'L'
+  end
+
+  seat
+end
+
+def calculate_seats(grid)
+  occupied_seats = 0
+  new_grid = Marshal.load(Marshal.dump(grid))
+
+  grid.each_with_index do |row, row_index|
+    row.each_with_index do |column, column_index|
+      new_seat = calculate_seat(row_index, column_index, grid)
+      new_grid[row_index][column_index] = new_seat
+      occupied_seats += 1 if new_seat == '#'
+    end
+  end
+
+  [occupied_seats, new_grid]
+end
+
+def compare_grid(grid1, grid2)
+  grid1.each_with_index do |row, row_index|
+    row.each_with_index do |column, column_index|
+      if grid1[row_index][column_index] != grid2[row_index][column_index]
+        return false
+      end
+    end
+  end
+
+  true
+end
+
+def find_occupied_seats(grid)
+  previous_grid = grid
+
+  while true
+    occupied_seats, new_grid = calculate_seats(previous_grid)
+    return occupied_seats if compare_grid(previous_grid, new_grid)
+    previous_grid = new_grid
+  end
+end
+
+puts find_occupied_seats(GRID)
