d57be5bb10d9dddb907e655b602a366042c0f8aa
david.adams <david.adams@gusto.com>
2020-12-14 18:45:06 -0700

    add 2020 day 14 solution

---
+++ 2020/day_14/part1.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+instructions = File.readlines('./input.txt', chomp: true).map do |line|
+  instruction = {}
+  instruction['mask'] = line.split('mask = ')[1] if line.include?('mask')
+  if line.include?('mem')
+    instruction['memory_address'] = line.scan(/\[(\d+)\]/).first.first.to_i
+    instruction['value'] = line.split(' = ')[1].to_i
+  end
+  instruction
+end
+
+def bitmask(value, mask)
+  value = format('%036d', value.to_s(2)).split('')
+
+  (value.size - 1).downto(0) do |index|
+    next if mask[index] == 'X'
+
+    value[index] = mask[index]
+  end
+
+  value.join.to_i(2)
+end
+
+mask = nil
+memory = {}
+
+instructions.each do |instruction|
+  if instruction['mask']
+    mask = instruction['mask']
+    next
+  end
+
+  memory_address = instruction['memory_address']
+  value = instruction['value']
+
+  memory[memory_address] = bitmask(value, mask)
+end
+
+answer = memory.values.sum
+puts answer
---
+++ 2020/day_14/part2.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+instructions = File.readlines('./input.txt', chomp: true).map do |line|
+  instruction = {}
+  instruction['mask'] = line.split('mask = ')[1] if line.include?('mask')
+  if line.include?('mem')
+    instruction['memory_address'] = line.scan(/\[(\d+)\]/).first.first.to_i
+    instruction['value'] = line.split(' = ')[1].to_i
+  end
+  instruction
+end
+
+def bitmask(address, mask)
+  binary_address = format('%036d', address.to_s(2)).split('')
+
+  (binary_address.size - 1).downto(0) do |index|
+    next if mask[index] == '0'
+
+    binary_address[index] = mask[index]
+  end
+
+  binary_address.join
+end
+
+def get_addresses(address)
+  floating_bits_count = address.count('X')
+  permutations = [0, 1].repeated_permutation(floating_bits_count)
+
+  permutations.map do |permutation|
+    copy_address = address.clone
+
+    permutation.map do |bit|
+      floating_index = copy_address.index('X')
+      copy_address[floating_index] = bit.to_s
+    end
+
+    copy_address.to_i(2)
+  end
+end
+
+mask = nil
+memory = {}
+
+instructions.each do |instruction|
+  if instruction['mask']
+    mask = instruction['mask']
+    next
+  end
+
+  memory_address = instruction['memory_address']
+  value = instruction['value']
+  masked_memory_address = bitmask(memory_address, mask)
+  memory_addresses = get_addresses(masked_memory_address)
+  memory_addresses.each { |address| memory[address] = value }
+end
+
+answer = memory.values.sum
+puts answer
