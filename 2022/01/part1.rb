e20f05d487d2af6e93db530c68e757a440a6ebd0
david.adams <david.adams@gusto.com>
2022-12-01 13:33:28 -0700

    Test rubocop

--- 2022/01/part1.rb
+++ 2022/01/part1.rb
@@ -5,7 +5,7 @@ elves = ARGF.read.split("\n\n").map(&:split)
 answer = elves.reduce(0) do |max_calories, calories|
   calories = calories.map(&:to_i).sum
 
-  (calories > max_calories) ? calories : max_calories
+  calories > max_calories ? calories : max_calories
 end
 
 puts "answer: #{answer}"
