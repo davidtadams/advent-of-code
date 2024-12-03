34c199e6a6c41994b8e542b1ac832f051a0cd2d6
David Adams <adams.t.david@gmail.com>
2024-12-03 12:04:33 -0700

    Add 2024 day 2 solution

---
+++ 2024/02/part2.js
@@ -0,0 +1,48 @@
+const fs = require('node:fs');
+const R = require('remeda');
+
+const path = process.argv[2] || 'simple_input.txt';
+
+const input = fs.readFileSync(path, 'utf-8').trim();
+const reports = input.split('\n').map(line => line.split(' ').map(Number));
+
+const isReportSafe = (report) => {
+  const sortedReport = [...report].sort((a, b) => a - b);
+  const reverseSortedReport = [...sortedReport].reverse();
+
+  if (!R.isShallowEqual(report, sortedReport) && !R.isShallowEqual(report, reverseSortedReport)) {
+    return false;
+  }
+
+  for (let i = 0; i < report.length - 1; i++) {
+    const difference = Math.abs(report[i] - report[i + 1]);
+
+    if (difference === 0 || difference > 3) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isReportSafeWithRemoval = (report) => {
+  for (let i = 0; i < report.length; i++) {
+    const reportWithRemoval = report.filter((_, index) => index !== i);
+
+    if (isReportSafe(reportWithRemoval)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+const answer = reports.reduce((count, report) => {
+  if (isReportSafe(report) || isReportSafeWithRemoval(report)) {
+    count += 1;
+  }
+
+  return count;
+}, 0);
+
+console.log('answer', answer);
